cf307534ce9b15f4df3be0ab85a70f7e8e7d1455
Gayatri Gopavajhala <gayatri924@gmail.com>
2020-02-05 15:57:19 -0500

    Fixed number counter + color switches

--- app/src/main/java/com/gopavajhalagayatri/buttonlab/MainActivity.java
+++ app/src/main/java/com/gopavajhalagayatri/buttonlab/MainActivity.java
@@ -3,6 +3,8 @@ package com.gopavajhalagayatri.buttonlab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     Button colorButton;
     EditText responseText;
     TextView displayText;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +33,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 System.out.println("Hi!!!");
                 Log.d("testButton","input is: "+responseText.getText());
-                displayText.setText(responseText.getText());
+                count += 1;
+                displayText.setText(responseText.getText() + "Click: " + count);
             }
         });
         colorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("colorButton","here");
-                colorButton.setBackgroundColor(Color.rgb(204, 255, 153));
+                ColorDrawable buttonColor = (ColorDrawable) colorButton.getBackground();
+                int colorId = buttonColor.getColor();
+                Log.d("colorButton","colorId: "+ colorId);
+                if(colorId == getResources().getColor(R.color.LightGreen)) {
+                    colorButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                }else{
+                    colorButton.setBackgroundColor(getResources().getColor(R.color.LightGreen));
+                }
             }
         });
     }
